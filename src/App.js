d67b63b6dee964fd0b0792df98bb16cedd0b9984
Татьяна Соснина <tatyana-sosnina@mail.ru>
2021-01-02 18:44:20 +0700

    style app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p style={{
-          color: 'red'
+          fontSize: 5 + 15
         }}>
         Hello World!
         </p>
